7080ba3e65a6a4eec04a303515ae27ae55860dbe
unknown <a.caaqil20@gmail.com>
2023-02-08 18:53:42 +0300

    api-adds-second-01

--- API/routers/route_survey.js
+++ API/routers/route_survey.js
@@ -64,45 +64,117 @@ router.get("/", async (req, res) => {
 router.get("/members", async (req, res) => {
   var query = survey_route.find();
   query.count(function (err, count) {
-    if (err)  res.status(500).json({ data: "error" });
-    else  res.status(500).json({ data: count });;
+    if (err) res.status(500).json({ data: "error" });
+    else res.status(500).json({ data: count });
   });
 });
 
-router.get("/count_data", async (req, res) => {
+const countRates = async () => {
+  const rates = [1, 2, 3, 4];
+  const counts = [];
+  const rateNames = {
+    1: "excellent",
+    2: "very_good",
+    3: "good",
+    4: "bad",
+  };
 
-  let rate_excellent = await survey_route.aggregate([
-    {
-        $match:{
-            rate: 1
-        }
-    },
-    {
-        $group:{
-            _id:null,
-            excellent: {$sum:'$rate'}
-        }
-    }
-])
-  let rate_very_good = await survey_route.aggregate([
-    {
-        $match:{
-            rate: 2
-        }
-    },
-    {
-        $group:{
-            _id:null,
-            veryGood: {$sum:'$rate'}
-        }
-    }
-])
+  for (const rate of rates) {
+    const count = await survey_route.countDocuments({ rate });
+    counts.push({ rate: rateNames[rate], count });
+    // counts.push({ rate, count });
+  }
 
+  return counts;
+};
 
+router.get("/filter_count", async (req, res) => {
+  try {
+    const counts = await countRates();
+    res.status(200).json(counts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error counting surveys" });
+  }
+  
 
-return res.status(200).json({ data:rate_excellent});
-
+ 
+  
 });
+// router.get("/filter_count", async (req, res) => {
+//   // let rate_excellent = await 
+
+//   // let rate_excellent = await survey_route.aggregate([
+//   //   {
+//   //     $match: {
+//   //       rate: 1,
+//   //     },
+//   //   },
+//   //   {
+//   //     $group: {
+//   //       _id: null,
+//   //       excellent: { $sum: "$rate" },
+//   //     },
+//   //   },
+//   // ]);
+//   let rate_very_good = await survey_route.aggregate([
+//     {
+//       $match: {
+//         rate: 2,
+//       },
+//     },
+//     {
+//       $group: {
+//         _id: null,
+//         veryGood: { $sum: "$rate" },
+//       },
+//     },
+//   ]);
+//   let rate_Good = await survey_route.aggregate([
+//     {
+//       $match: {
+//         rate: 3,
+//       },
+//     },
+//     {
+//       $group: {
+//         _id: null,
+//         Good: { $sum: "$rate" },
+//       },
+//     },
+//   ]);
+//   let rate_bad = await survey_route.aggregate([
+//     {
+//       $match: {
+//         rate: 4,
+//       },
+//     },
+//     {
+//       $group: {
+//         _id: null,
+//         bad: { $sum: "$rate" },
+//       },
+//     },
+//   ]);
+
+//   data = [
+//     {
+//       excellent: rate_excellent.length == 0 ? 0 : rate_excellent[0].excellent,
+//     },
+//     {
+//       veryGood: rate_very_good.length == 0 ? 0 : rate_very_good[0].veryGood,
+//     },
+//     {
+//       Good: rate_Good.length == 0 ? 0 : rate_Good[0].Good,
+//     },
+//     {
+//       bad: rate_bad.length == 0 ? 0 : rate_bad[0].Good,
+//     },
+//   ];
+
+//   return res.status(200).json(data);
+  
+// });
 
 router.post("/post", async (req, res) => {
   //   const data = await req.body;
