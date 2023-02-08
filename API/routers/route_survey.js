f0fb87f13b41c7a5490214f96206456eb03da273
unknown <a.caaqil20@gmail.com>
2023-02-08 19:02:46 +0300

    api-adds-second-02-filter-api

--- API/routers/route_survey.js
+++ API/routers/route_survey.js
@@ -68,6 +68,24 @@ router.get("/members", async (req, res) => {
     else res.status(500).json({ data: count });
   });
 });
+router.get("/filter", async (req, res) => {
+  rate_id= req.body.rate;
+  
+  const surveyData = await survey_route
+  .find({rate:rate_id})
+  .sort({ createdAt: -1 })
+  .limit(5);
+try {
+  if (surveyData) {
+    return res.status(200).json({ data: surveyData });
+  }
+  res.status(404).json({ data: "Not found users" });
+  return;
+} catch (error) {
+  res.status(500).json({ data: error });
+  return;
+}
+});
 
 const countRates = async () => {
   const rates = [1, 2, 3, 4];
@@ -101,80 +119,7 @@ router.get("/filter_count", async (req, res) => {
  
   
 });
-// router.get("/filter_count", async (req, res) => {
-//   // let rate_excellent = await 
 
-//   // let rate_excellent = await survey_route.aggregate([
-//   //   {
-//   //     $match: {
-//   //       rate: 1,
-//   //     },
-//   //   },
-//   //   {
-//   //     $group: {
-//   //       _id: null,
-//   //       excellent: { $sum: "$rate" },
-//   //     },
-//   //   },
-//   // ]);
-//   let rate_very_good = await survey_route.aggregate([
-//     {
-//       $match: {
-//         rate: 2,
-//       },
-//     },
-//     {
-//       $group: {
-//         _id: null,
-//         veryGood: { $sum: "$rate" },
-//       },
-//     },
-//   ]);
-//   let rate_Good = await survey_route.aggregate([
-//     {
-//       $match: {
-//         rate: 3,
-//       },
-//     },
-//     {
-//       $group: {
-//         _id: null,
-//         Good: { $sum: "$rate" },
-//       },
-//     },
-//   ]);
-//   let rate_bad = await survey_route.aggregate([
-//     {
-//       $match: {
-//         rate: 4,
-//       },
-//     },
-//     {
-//       $group: {
-//         _id: null,
-//         bad: { $sum: "$rate" },
-//       },
-//     },
-//   ]);
-
-//   data = [
-//     {
-//       excellent: rate_excellent.length == 0 ? 0 : rate_excellent[0].excellent,
-//     },
-//     {
-//       veryGood: rate_very_good.length == 0 ? 0 : rate_very_good[0].veryGood,
-//     },
-//     {
-//       Good: rate_Good.length == 0 ? 0 : rate_Good[0].Good,
-//     },
-//     {
-//       bad: rate_bad.length == 0 ? 0 : rate_bad[0].Good,
-//     },
-//   ];
-
-//   return res.status(200).json(data);
-  
-// });
 
 router.post("/post", async (req, res) => {
   //   const data = await req.body;
